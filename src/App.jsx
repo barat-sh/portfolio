7f1259f7be7ca38641b10b4da8085fdb71ca7dbf
barat-sh <barath.elangovan@icloud.com>
2024-08-17 10:53:57 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import NavBar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import Work from "./components/Work.jsx";
 
 function App() {
-
   return (
     <>
+            <BrowserRouter>
+                <NavBar>
+                    <Routes>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/work" element={<Work/>} />
+                        {/*<Route path="/education" element={<Home/>} />*/}
+                        {/*<Route path="/blog" element={<Home/>} />*/}
+                        {/*<Route path="/contact" element={<Home/>} />*/}
+                    </Routes>
+                </NavBar>
+            </BrowserRouter>
     </>
   )
 }
