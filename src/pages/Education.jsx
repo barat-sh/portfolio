f830dcaf0d0ff604a96f9a6a4e666f291d54e973
Barath Elangovan <barath.elangovan@icloud.com>
2024-08-22 00:52:48 +0530

    page contents added

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -7,80 +7,56 @@ const Education = () => {
     }, []);
 
     return (
-        <>
-            <div className="flex flex-col text-justify md:max-w-2xl mt-4 py-2 space-y-4 text-gray-200">
-                <p className="flex w-auto pt-6 text-3xl text-gray-100">
+        <main className="mb-32">
+            <div className="flex flex-col text-justify md:max-w-2xl mt-4 py-2 space-y-4">
+                <p className="flex font-mono w-auto pt-6 text-xl text-neutral-300">
                     my education
                 </p>
-                <p className="flex text-justify w-auto pb-4 pt-2 text-gray-300">
+                <p className="flex text-justify w-auto pb-4 pt-2 text-stone-300">
                     On a mission to build products developers love, and along the way, teach the next generation of
                     developers. Heres a summary of my work so far.
                 </p>
             </div>
             <div style={{borderTop: '1px solid gray', margin: '10px 0'}}/>
-            <div className="flex flex-col text-justify md:max-w-2xl py-2 space-y-4 text-gray-200">
+            <div
+                className="flex justify-between text-justify mt-6 max-w-2xl min-w-[440px] md:min-w-[500px] mb-6 w-auto h-auto min-h-32 shadow-md rounded-lg space-y-4 text-gray-200 border border-stone-700 p-4 border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-gray-600 hover:dark:bg-neutral-800/30">
                 <div>
-                    <p className="flex w-auto pt-6 text-xl text-gray-100">
+                    <p className="flex w-auto pt-6 text-xl text-neutral-300">
                         Woolf University
                     </p>
                     <p className="flex w-auto text-sm text-gray-500">
                         Masters in Computer Science
                     </p>
                 </div>
-                <p className="flex text-justify w-auto pb-4">
-                    I joined Vercel early to grow Next.js and our developer community. I built our Developer Relations
-                    team to teach our community about our products.
-                </p>
-                <ul className="list-disc list-inside ml-4">
-                    <li className="mb-2">
-                        In 2021, I was promoted to Director of DevRel. We translated customer pain back into the product
-                        roadmap. We spoke at conferences, wrote blog posts, and created videos. We built open-source
-                        examples and contributed back to the product.
-                    </li>
-                    <li className="mb-2">
-                        In 2022, I was promoted to VP of Developer Experience, now also leading our product
-                        documentation team. My team created a new free course to teach Next.js, which had 17 million
-                        page views that year. I shipped. I partnered with our open-source community, collaborating with
-                        frameworks like Nuxt and Astro and sponsoring individuals and projects.
-                    </li>
-                    <li className="mb-2">
-                        In 2023, I was promoted to VP of Product, now also leading Product Management.
-                    </li>
-                </ul>
+                <span className="text-neutral-400">90 credits</span>
             </div>
-            <div style={{borderTop: '1px solid gray', margin: '10px 0'}}/>
-            <div className="flex flex-col text-justify md:max-w-2xl py-2 space-y-4 text-gray-200">
+            <div
+                className="flex justify-between text-justify max-w-2xl min-w-[440px] md:min-w-[500px] mb-6 w-auto h-auto min-h-32 shadow-md rounded-lg space-y-4 text-gray-200 border border-stone-700 p-4 border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-gray-600 hover:dark:bg-neutral-800/30">
                 <div>
-                    <p className="flex w-auto pt-6 text-xl text-gray-100">
+                    <p className="flex w-auto pt-6 text-xl text-neutral-300">
                         BIT - Erode
                     </p>
                     <p className="flex w-auto text-sm text-gray-500">
                         Bachelors in Electronics and Instrumentation. (spec: Machine Coding)
                     </p>
                 </div>
-                <p className="flex text-justify w-auto pb-4">
-                    I joined Vercel early to grow Next.js and our developer community. I built our Developer Relations
-                    team to teach our community about our products.
-                </p>
-                <ul className="list-disc list-inside ml-4">
-                    <li className="mb-2">
-                        In 2021, I was promoted to Director of DevRel. We translated customer pain back into the product
-                        roadmap. We spoke at conferences, wrote blog posts, and created videos. We built open-source
-                        examples and contributed back to the product.
-                    </li>
-                    <li className="mb-2">
-                        In 2022, I was promoted to VP of Developer Experience, now also leading our product
-                        documentation team. My team created a new free course to teach Next.js, which had 17 million
-                        page views that year. I shipped. I partnered with our open-source community, collaborating with
-                        frameworks like Nuxt and Astro and sponsoring individuals and projects.
-                    </li>
-                    <li className="mb-2">
-                        In 2023, I was promoted to VP of Product, now also leading Product Management.
-                    </li>
-                </ul>
+                <span className="text-neutral-400">120 credits</span>
             </div>
             <div style={{borderTop: '1px solid gray', margin: '10px 0'}}/>
-        </>
+            <div className="flex space-x-4 my-10">
+                <a
+                    href="https://x.com/_barat_tw"
+                    className="flex p-3 px-5 md:px-8 rounded-lg border border-neutral-800 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-gray-600 hover:dark:bg-neutral-800/30">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                         stroke="currentColor" className="size-[1.2rem] mt-[0.4rem] mr-2">
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                              d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"/>
+                    </svg>
+                    <span className="">follow me</span>
+                </a>
+            </div>
+
+        </main>
     )
 }
 
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -8,107 +8,133 @@ const Work = () => {
 
     return (
         <main className="mb-32">
-            <div className="flex flex-col text-justify md:max-w-2xl mt-4 py-2 space-y-4 text-gray-200">
-                <p className="flex w-auto pt-6 text-3xl text-gray-100">
+            <div className="flex flex-col text-justify md:max-w-2xl mt-4 py-2 space-y-4">
+                <p className="flex font-mono w-auto pt-6 text-xl text-neutral-300">
                     my work
                 </p>
-                <p className="flex text-justify w-auto pb-4 pt-2 text-gray-300">
-                    On a mission to build products developers love, and along the way, teach the next generation of
-                    developers. Heres a summary of my work so far.
+                <p className="flex text-justify w-auto pb-4 pt-2 text-stone-300">
+                    On a mission to build products, and along the way, teach the next generation of
+                    developers. Here&apos;s a summary of my work so far.
                 </p>
             </div>
             <div style={{borderTop: '1px solid gray', margin: '10px 0'}}/>
-            <div className="flex flex-col text-justify md:max-w-2xl space-y-4 text-gray-200">
-                <div>
-                    <p className="flex w-auto pt-6 text-xl text-gray-100">
-                        Turbohire
-                    </p>
-                    <p className="flex w-auto text-sm text-gray-500">
-                        Junior Software Engineer
-                    </p>
+            <div className="flex flex-col text-justify md:max-w-2xl space-y-4 text-neutral-400">
+                <div className="flex mt-6 justify-between">
+                    <div>
+                        <div className="flex">
+                            <a href={"https://turbohire.co"}>
+                                <img className="w-6 h-6 mr-2 bg-neutral-400" alt="turbohire"
+                                     src="https://turbohire.co/wp-content/uploads/2023/01/cropped-favicon-192x192.png"/>
+                            </a>
+                            <a href="https://turbohire.co" className="text-xl font-mono text-neutral-300 hover:text-neutral-200">
+                                Turbohire
+                            </a>
+                        </div>
+                        <p className="flex w-auto text-sm text-gray-500">
+                            Jr. Software Engineer
+                        </p>
+                    </div>
+                    <div>
+                        <span>(1-Y)</span>
+                    </div>
                 </div>
-                <p className="flex text-justify w-auto pb-4">
-                    I joined Vercel early to grow Next.js and our developer community. I built our Developer Relations
-                    team to teach our community about our products.
+                <p className="text-justify w-auto pb-2">
+                    TurboHire is globally compliant for all hiring, a Complete Hiring Solution for Enterprises. where, I
+                    primarily
+                    collaborated with the backend team implementing APIs, also in frontend team during production
+                    release to fast-forward work environment.
                 </p>
                 <ul className="list-disc list-inside ml-4">
                     <li className="mb-2">
-                        In 2021, I was promoted to Director of DevRel. We translated customer pain back into the product
-                        roadmap. We spoke at conferences, wrote blog posts, and created videos. We built open-source
-                        examples and contributed back to the product.
+                        In 2023, I joined as SDE intern, I Achieved an significant increase in Unit testing code
+                        coverage from <i className="font-bold">20%</i> to nearly <i
+                        className="font-bold">85%</i> through
+                        the implementation of
+                        <a href={"https://nunit.org/"} className="underline inline-flex ml-2 mr-[3px]">
+                            <img className="w-5 h-5 bg-neutral-900" alt="turbohire"
+                                 src="https://nunit.org/img/nunit.svg"/>
+                        </a>
+                        <a className="underline text-neutral-300 mr-2" href={"https://nunit.org/"}>NUnit</a>
+                        . Improved code coverage resulted in
+                        reducing post-release defects. via writing unit test, I explored the large codebase(all the
+                        services).
                     </li>
                     <li className="mb-2">
-                        In 2022, I was promoted to VP of Developer Experience, now also leading our product
-                        documentation team. My team created a new free course to teach Next.js, which had 17 million
-                        page views that year. I shipped. I partnered with our open-source community, collaborating with
-                        frameworks like Nuxt and Astro and sponsoring individuals and projects.
+                        Later last year, I was promoted to SDE1, We Designed and implemented <i className="font-bold">Activity
+                        Timeline</i> feature across the platform by achieving maintainable/scalable code with handling
+                        large data by making simultaneous API calls and ensuring robust error handling, leading to
+                        massive improvement in customer-satisfaction scores.
                     </li>
                     <li className="mb-2">
-                        In 2023, I was promoted to VP of Product, now also leading Product Management.
+                        I resolved over <i className="font-bold">800 bugs</i>, ensuring the platform&apos;s stability
+                        and performance. In addition, I implemented key client-requested features, such as <i
+                        className="font-bold">time tracking</i> across the platform, <i className="font-bold">advanced
+                        filtering options</i>, a fully <i className="font-bold">responsive UI</i>, and page-specific
+                        functionalities. These contributions not only enhanced the user experience but also aligned the
+                        product more closely with client needs.
+                    </li>
+                </ul>
+                <p className="text-justify w-auto pb-2">
+                    Since I joined
+                    <a href={"https://turbohire.co"} className="underline inline-flex ml-2 mr-[3px]">
+                        <img className="w-4 h-4 bg-neutral-400" alt="turbohire"
+                             src="https://turbohire.co/wp-content/uploads/2023/01/cropped-favicon-192x192.png"/>
+                    </a>
+                    <a className="underline text-neutral-300 mr-2" href={"https://turbohire.co"}>Turbohire</a>
+                    in 2023, My team contributions not only enhanced the user experience but also aligned the product
+                    more closely with client needs, demonstrating my ability to deliver impactful solutions under tight
+                    deadlines. It&apos;s used by Ola, Lenskart, PineLabs, Wakefit, Accel, Hanu, and more.
+                </p>
+            </div>
+            <div style={{borderTop: '1px solid gray', margin: '10px 0'}}/>
+            <div className="flex flex-col text-justify md:max-w-2xl space-y-4 text-neutral-400">
+                <div className="flex mt-6 justify-between">
+                    <div>
+                        <div className="flex">
+                            <a href="https://100xdevs.com/" className="text-xl font-mono text-neutral-300 hover:text-neutral-200">
+                                100x-Devs Cohort
+                            </a>
+                        </div>
+                        <p className="flex w-auto text-sm text-gray-500">
+                            Teaching Assistent and <i className="font-bold ml-1 text-gray-400">Student Mentor</i>
+                        </p>
+                    </div>
+                    <div>
+                        <span>(8-M)</span>
+                    </div>
+                </div>
+                <p className="text-justify w-auto pb-2">
+                    100x-devs is an initiative cohort by <a className="underline text-stone-400 hover:text-stone-300 mr-2" href="https://x.com/kirat_tw?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">Harkirat Singh</a>. Helping folks in community to understand technologies deep, starting from basics to able to contribute in open-source projects.
+                </p>
+                <ul className="list-disc list-inside ml-4">
+                    <li className="mb-2">
+                        In mid 2023, I joined as Teaching assistant, guided peers in MERN stack. Played a role in clarifying concepts, troubleshooting coding challenges, with a collaborative learning environment.
+                    </li>
+                    <li className="mb-2">
+                        Later 2023, I was volunteered as Student mentor, helps in deep understanding of DOM Manipulation, Promises, Hooks, State Management, Context API, Recoil, RDBMS, version control, API Design, Mono-repos, Server-Side Render, Rest APIs ORM(Prisma), CI/CD in full stack.
                     </li>
                 </ul>
             </div>
             <div style={{borderTop: '1px solid gray', margin: '10px 0'}}/>
-            <div className="flex flex-col text-justify md:max-w-2xl space-y-4 text-gray-200">
-                <div>
-                    <p className="flex w-auto pt-6 text-xl text-gray-100">
-                        100x-Devs Cohort
-                    </p>
-                    <p className="flex w-auto text-sm text-gray-500">
-                        Teaching Assistent and Student Mentor
-                    </p>
+            <div className="flex flex-col text-justify md:max-w-2xl space-y-4 text-neutral-400">
+                <div className="flex mt-6 justify-between">
+                    <div>
+                        <div className="flex">
+                            <a href="https://www.interviewbit.com" className="text-xl font-mono text-neutral-300 hover:text-neutral-200">
+                                InterviewBit
+                            </a>
+                        </div>
+                        <p className="flex w-auto text-sm text-gray-500">
+                            Software Engineer Intern
+                        </p>
+                    </div>
+                    <div>
+                        <span>(6-M)</span>
+                    </div>
                 </div>
-                <p className="flex text-justify w-auto pb-4">
-                    I joined Vercel early to grow Next.js and our developer community. I built our Developer Relations
-                    team to teach our community about our products.
+                <p className="text-justify w-auto pb-2">
+                    InterviewBit is an ed-tech, The platform is designed to help prepare for interviews at top tech companies. During my time at InterviewBit, I gained my first production experience in Industry. I worked on contest, testcase and mock to help improve quality of product and automate the contest happens every week, building a product similar to open-source platform like Leetcode.
                 </p>
-                <ul className="list-disc list-inside ml-4">
-                    <li className="mb-2">
-                        In 2021, I was promoted to Director of DevRel. We translated customer pain back into the product
-                        roadmap. We spoke at conferences, wrote blog posts, and created videos. We built open-source
-                        examples and contributed back to the product.
-                    </li>
-                    <li className="mb-2">
-                        In 2022, I was promoted to VP of Developer Experience, now also leading our product
-                        documentation team. My team created a new free course to teach Next.js, which had 17 million
-                        page views that year. I shipped. I partnered with our open-source community, collaborating with
-                        frameworks like Nuxt and Astro and sponsoring individuals and projects.
-                    </li>
-                    <li className="mb-2">
-                        In 2023, I was promoted to VP of Product, now also leading Product Management.
-                    </li>
-                </ul>
-            </div>
-            <div style={{borderTop: '1px solid gray', margin: '10px 0'}}/>
-            <div className="flex flex-col text-justify md:max-w-2xl space-y-4 text-gray-200">
-                <div>
-                    <p className="flex w-auto pt-6 text-xl text-gray-100">
-                        InterviewBit
-                    </p>
-                    <p className="flex w-auto text-sm text-gray-500">
-                        Software Engineer Intern
-                    </p>
-                </div>
-                <p className="flex text-justify w-auto pb-4">
-                    I joined Vercel early to grow Next.js and our developer community. I built our Developer Relations
-                    team to teach our community about our products.
-                </p>
-                <ul className="list-disc list-inside ml-4">
-                    <li className="mb-2">
-                        In 2021, I was promoted to Director of DevRel. We translated customer pain back into the product
-                        roadmap. We spoke at conferences, wrote blog posts, and created videos. We built open-source
-                        examples and contributed back to the product.
-                    </li>
-                    <li className="mb-2">
-                        In 2022, I was promoted to VP of Developer Experience, now also leading our product
-                        documentation team. My team created a new free course to teach Next.js, which had 17 million
-                        page views that year. I shipped. I partnered with our open-source community, collaborating with
-                        frameworks like Nuxt and Astro and sponsoring individuals and projects.
-                    </li>
-                    <li className="mb-2">
-                        In 2023, I was promoted to VP of Product, now also leading Product Management.
-                    </li>
-                </ul>
             </div>
             <div style={{borderTop: '1px solid gray', margin: '10px 0'}}/>
             <div className="flex space-x-4 my-10">
@@ -120,18 +146,19 @@ const Work = () => {
                         <path strokeLinecap="round" strokeLinejoin="round"
                               d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"/>
                     </svg>
-                    <span className="">follow me</span>
+                    <span className="font-mono">follow me</span>
                 </a>
-                <a
-                    href="https://leetcode.com/u/barathelangovan/"
+                <button
+                    disabled={true}
+                    // href="https://leetcode.com/u/barathelangovan/"
                     className="flex p-3 px-5 md:px-8 rounded-lg border border-neutral-800 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-gray-600 hover:dark:bg-neutral-800/30">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                          stroke="currentColor" className="size-[1.2rem] mt-[0.4rem] mr-2">
                         <path strokeLinecap="round" strokeLinejoin="round"
                               d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"/>
                     </svg>
-                    <span className="">leetcode</span>
-                </a>
+                    <span className="font-mono">leetcode</span>
+                </button>
             </div>
 
         </main>
